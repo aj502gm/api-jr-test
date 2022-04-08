226fb884a12a0d3c8decc0d72ebfb1908bc89dd0
ajgm502 <andres.gonzalez@ext.firstlight.ai>
2022-04-08 13:53:32 -0600

    Missing header

--- backend-api/server.js
+++ backend-api/server.js
@@ -10,6 +10,7 @@ const { getClients } = require('./DataModels/Clients/getClients');
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();    
 })
 
